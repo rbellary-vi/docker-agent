7aa006ba9c0ec48808bfa3e514f3cdff1f792d12
Shawn Butts <me@shawnbutts.com>
2015-08-05 12:21:20 -0400

    updated to the latest version of the agent
    this has the side affect of reduce the image size

--- HISTORY.md
+++ HISTORY.md
@@ -1,6 +1,11 @@
 Netuitive Docker Agent Release History
 ======================================
 
+Version 0.1.6 - Aug XX XXXX
+---------------------------
+- reduce size of image
+- updated to the latest version of the agent
+
 Version 0.1.4 - Jul 01 2015
 ---------------------------
 - initial release
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,14 @@
 set -e
 echo "=> Configuring Netuitive Agent"
 
-cat /etc/diamond/netuitive-agent.conf.tpl|sed -e "s/https/${HTTPVAR}/g"|sed -e "s/INFO/${LOGLEVEL}/g"|sed -e "s/interval\ =\ 60/interval\ =\ ${INTERVAL}/g"|sed -e "s/<custom\ datasource\ api\ key>/${APIKEY}/g"|sed -e "s/api\.app\.netuitive\.com/${APIHOST}/g"|sed -e "s/# hostname = my_custom_hostname/hostname = ${DOCKER_HOSTNAME}/g" > /etc/diamond/netuitive-agent.conf
+cat /opt/netuitive-agent/conf/netuitive-agent.conf.tpl|sed -e "s/https/${HTTPVAR}/g"|sed -e "s/INFO/${LOGLEVEL}/g"|sed -e "s/interval\ =\ 60/interval\ =\ ${INTERVAL}/g"|sed -e "s/<custom\ datasource\ api\ key>/${APIKEY}/g"|sed -e "s/api\.app\.netuitive\.com/${APIHOST}/g"|sed -e "s/# hostname = my_custom_hostname/hostname = ${DOCKER_HOSTNAME}/g" > /opt/netuitive-agent/conf/netuitive-agent.conf
 
-chmod 644 /etc/diamond/netuitive-agent.conf
+chmod 644 /opt/netuitive-agent/conf/netuitive-agent.conf
 
-cat /usr/local/lib/python2.7/site-packages/diamond/utils/log.py.tpl|sed -e "s/logging\.DEBUG/logging\.${LOGLEVEL}/g" > /usr/local/lib/python2.7/site-packages/diamond/utils/log.py
+cat /opt/netuitive-agent/embedded/lib/python2.7/site-packages/diamond/utils/log.py.tpl|sed -e "s/logging\.DEBUG/logging\.${LOGLEVEL}/g" > /opt/netuitive-agent/embedded/lib/python2.7/site-packages/diamond/utils/log.py
 
 echo "=> Done!"
 
+export SENSORS_LIB=/opt/netuitive-agent/embedded/lib/libsensors.so
 
-/usr/local/bin/python2.7 /usr/local/bin/diamond --skip-pidfile --configfile=/etc/diamond/netuitive-agent.conf --log-stdout --foreground
+/opt/netuitive-agent/embedded/bin/python /opt/netuitive-agent/bin/netuitive-agent --configfile=/opt/netuitive-agent/conf/netuitive-agent.conf --log-stdout --foreground
