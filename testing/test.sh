9ffd04be336c34e48928aad32567d994e1c1d65c
shawnbutts <me@shawnbutts.com>
2015-12-03 14:20:37 -0500

    update test script

--- testing/test.sh
+++ testing/test.sh
@@ -8,7 +8,7 @@ time docker build -t netuitive-agent-api-test .
 docker run -d -h testserver -p 8000:8000 -v `pwd`:/vagrant:rw --name netuitive-agent-api-test netuitive-agent-api-test
 sleep 2
 
-docker run -d  --link netuitive-agent-api-test --name netuitive-agent-test -e DOCKER_HOSTNAME=testserver -e APIKEY=testapikey -e HTTPVAR=http -e LOGLEVEL=DEBUG -e APIHOST=$(docker inspect netuitive-agent-api-test | grep IPAddress | cut -d '"' -f 4):8000 -v /proc:/host_proc:ro -v /var/run/docker.sock:/var/run/docker.sock:ro netuitive/docker-agent
+docker run -d  --link netuitive-agent-api-test --name netuitive-agent-test -e DOCKER_HOSTNAME=testserver -e APIKEY=testapikey -e HTTPVAR=http -e LOGLEVEL=DEBUG -e APIHOST=$(docker inspect netuitive-agent-api-test | grep IPAddress | cut -d '"' -f 4|tail -1):8000 -v /proc:/host_proc:ro -v /var/run/docker.sock:/var/run/docker.sock:ro netuitive/docker-agent
 
 sleep 90
 
