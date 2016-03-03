7f4bf7f4820a336dbca9cc69f7e8d38256c95a80
shawnbutts <me@shawnbutts.com>
2016-03-03 16:15:46 -0500

    fix api key replacment

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,44 +3,44 @@ set -e
 echo "Copying configs in place..."
 find /conf -name '*.*' -exec cp {} /opt/netuitive-agent/conf \;
 
-if [[ ! $USE_LOCAL_CONFIG ]]; then 
+if [[ ! $USE_LOCAL_CONFIG ]]; then
 	# if we don't already have a config file, copy over template (assume ENV variables will further config)
 	if [[ ! -r /opt/netuitive-agent/conf/netuitive-agent.conf ]]; then
 		echo "No config file found, copying over template file..."
 		cp /opt/netuitive-agent/conf/netuitive-agent.conf.tpl /opt/netuitive-agent/conf/netuitive-agent.conf
-	else 
+	else
 		echo "Found config file in /opt/netuitive-agent/conf/netuitive-agent.conf file, using..."
 	fi
 
 	echo "Checking for environment variable overrides..."
 	if [[ $APIKEY ]]; then
-		sed -i -e "s/<custom\ datasource\ api\ key>/${APIKEY}/g" /opt/netuitive-agent/conf/netuitive-agent.conf 
-		echo "Configuring APIKEY with $APIKEY" 
+		sed -i -e "s/<datasource\ api\ key>/${APIKEY}/g" /opt/netuitive-agent/conf/netuitive-agent.conf
+		echo "Configuring APIKEY with $APIKEY"
 	fi
 
-	if [[ $HTTPVAR ]]; then 
+	if [[ $HTTPVAR ]]; then
 		sed -i -e "s/https/${HTTPVAR}/g" /opt/netuitive-agent/conf/netuitive-agent.conf
-		echo "Configuring HTTPVAR with $HTTPVAR" 
-	fi 
-
-	if [[ $LOGLEVEL ]]; then 
-		sed -i -e "s/INFO/${LOGLEVEL}/g" /opt/netuitive-agent/conf/netuitive-agent.conf
-		echo "Configuring loglevel with $LOGLEVEL" 
-	fi 
-
-	if [[ $INTERVAL ]]; then 
-		sed -i -e "s/interval\ =\ 60/interval\ =\ ${INTERVAL}/g" /opt/netuitive-agent/conf/netuitive-agent.conf
-		echo "Configuring interval with $INTERVAL" 
-	fi 
-
-	if [[ $APIHOST ]]; then 
-		sed -i -e "s/api\.app\.netuitive\.com/${APIHOST}/g" /opt/netuitive-agent/conf/netuitive-agent.conf
-		echo "Configuring APIHOST with $APIHOST" 
+		echo "Configuring HTTPVAR with $HTTPVAR"
 	fi
 
-	if [[ $DOCKER_HOSTNAME ]]; then 
+	if [[ $LOGLEVEL ]]; then
+		sed -i -e "s/INFO/${LOGLEVEL}/g" /opt/netuitive-agent/conf/netuitive-agent.conf
+		echo "Configuring loglevel with $LOGLEVEL"
+	fi
+
+	if [[ $INTERVAL ]]; then
+		sed -i -e "s/interval\ =\ 60/interval\ =\ ${INTERVAL}/g" /opt/netuitive-agent/conf/netuitive-agent.conf
+		echo "Configuring interval with $INTERVAL"
+	fi
+
+	if [[ $APIHOST ]]; then
+		sed -i -e "s/api\.app\.netuitive\.com/${APIHOST}/g" /opt/netuitive-agent/conf/netuitive-agent.conf
+		echo "Configuring APIHOST with $APIHOST"
+	fi
+
+	if [[ $DOCKER_HOSTNAME ]]; then
 		sed -i -e "s/# hostname = my_custom_hostname/hostname = ${DOCKER_HOSTNAME}/g" /opt/netuitive-agent/conf/netuitive-agent.conf
-		echo "Configuring HOSTNAME with $DOCKER_HOSTNAME" 
+		echo "Configuring HOSTNAME with $DOCKER_HOSTNAME"
 	fi
 fi
 
