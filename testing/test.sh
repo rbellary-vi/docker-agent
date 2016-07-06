205cdf39f9d5dc84be44d66c3a47a18c1e6701d3
shawnbutts <shawnbutts@users.noreply.github.com>
2016-07-06 15:18:23 -0400

    make sure test image is removed first

--- testing/test.sh
+++ testing/test.sh
@@ -3,6 +3,13 @@
 rm -f testserver.log
 rm -f testserver.pass
 
+docker stop netuitive-agent-api-test; sleep 1
+docker stop netuitive-agent-test; sleep 1
+docker rm netuitive-agent-api-test; sleep 1
+docker rm netuitive-agent-test; sleep 1
+docker rmi netuitive-agent-api-test
+
+
 time docker build -t netuitive-agent-api-test .
 
 docker run -d -h testserver -p 8000:8000 -v `pwd`:/vagrant:rw --name netuitive-agent-api-test netuitive-agent-api-test
@@ -10,7 +17,7 @@ sleep 2
 
 TESTIP=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' netuitive-agent-api-test)
 
-docker run -d  --link netuitive-agent-api-test --name netuitive-agent-test -e DOCKER_HOSTNAME=testserver -e APIKEY=testapikey -e HTTPVAR=http -e LOGLEVEL=DEBUG -e APIHOST=$(TESTIP):8000 -v /proc:/host_proc:ro -v /var/run/docker.sock:/var/run/docker.sock:ro netuitive/docker-agent
+docker run -d  --link netuitive-agent-api-test --name netuitive-agent-test -e DOCKER_HOSTNAME=testserver -e APIKEY=testapikey -e HTTPVAR=http -e LOGLEVEL=DEBUG -e APIHOST=$TESTIP:8000 -v /proc:/host_proc:ro -v /var/run/docker.sock:/var/run/docker.sock:ro netuitive/docker-agent
 
 sleep 90
 
